81a5286ba5d48eeef544c38ba048b8a533afe585
mcidonch <>
2019-07-23 14:25:10 +0200

    Corrections

--- app/src/main/java/com/example/gangameiigo/model/TopGameMapper.kt
+++ app/src/main/java/com/example/gangameiigo/model/TopGameMapper.kt
@@ -1,14 +1,12 @@
 package com.example.gangameiigo.model
 
-import com.example.gangameiigo.R
-
 
 object TopGameMapper {
 
     fun fromSdk(game : com.example.mysdk.TopGame, position: Int) :TopGame {
         return if(0 == game.price.compareTo(0.0))
             TopGame(game.title, game.imageThumb, game.steamRating/10,
-                game.getOwner(), "Not for sale", game.designer, position)
+                game.getOwner(), "Free", game.designer, position)
 
          else TopGame(game.title, game.imageThumb, game.steamRating/10,
             game.getOwner(), "$"+(game.price/100).toString(), game.designer, position)
